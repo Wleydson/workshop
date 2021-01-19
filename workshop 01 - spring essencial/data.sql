74bf94cda110429cebb585d3906e3266bf4443df
Wleydson Lemos <wleydsonredes@gmail.com>
2021-01-19 14:43:22 -0300

    data sql

---
+++ workshop 01 - spring essencial/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO cliente (nome, cpf, email, telefone, data_registro, ativo) VALUES ('Wleydson', '000000000', 'meu@email.com', '+55 85 00000-0000', CURRENT_DATE, true );
+INSERT INTO cliente (nome, cpf, email, telefone, data_registro, ativo) VALUES ('Fulano', '000000000', 'fulano_email@email.com', '+55 85 00000-0000', CURRENT_DATE, true );
+INSERT INTO cliente (nome, cpf, email, telefone, data_registro, ativo) VALUES ('Ze', '000000000', 'ze_email@email.com', '+55 85 00000-0000', CURRENT_DATE, true );
+INSERT INTO cliente (nome, cpf, email, telefone, data_registro, ativo) VALUES ('João Carlos', '000000000', 'jc_emails@email.com', '+55 85 00000-0000', CURRENT_DATE, true );
+INSERT INTO cliente (nome, cpf, email, telefone, data_registro, ativo) VALUES ('Não faço ideia', '000000000', 'erro@email.com', '+55 85 00000-0000', CURRENT_DATE, true );
+
+
+INSERT INTO servicos (descricao, valor, cliente_id) VALUES ('Programar', 15000, 1);
+INSERT INTO servicos (descricao, valor, cliente_id) VALUES ('Trabalho extra', 9000, 1);
+INSERT INTO servicos (descricao, valor, cliente_id) VALUES ('Desing', 4000, 2);
+INSERT INTO servicos (descricao, valor, cliente_id) VALUES ('QA', 3500, 5);
+
+
